188e6cf135c5121c6280c746909e90c537c1445d
CrazyZitrone <87885404+CrazyZitrone@users.noreply.github.com>
2022-12-14 18:04:20 +0100

    main

--- main.js
+++ main.js
@@ -1,14 +1,7 @@
-    /* Navbar Javascript */
-    $('.navTrigger').click(function () {
-        $(this).toggleClass('active');
-        $("#mainListDiv").toggleClass("show_list");
-        $("#mainListDiv").fadeIn();
-    });
-    $(window).scroll(function() {
-        if ($(document).scrollTop() > 50) {
-            $('.nav').addClass('affix');
-        } else {
-            $('.nav').removeClass('affix');
-        }
-    });
-    
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
+});
