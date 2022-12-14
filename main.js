eabee83b554481551ae8c1fe7be6730f9439fc1b
CrazyZitrone <87885404+CrazyZitrone@users.noreply.github.com>
2022-12-14 21:51:31 +0100

    main

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 /* --Navbar-Click-menu-- */
 $('.navbarTrigger').click(function () {
     $(this).toggleClass('active');
-    console.log("Clicked menu");
     $("#navbarList").toggleClass("show_list");
     $("#navbarList").fadeIn();
 
